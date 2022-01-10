588980ad84974eb68b39a532abfc8d235b12a6a7
Gabriel <contatogabrielgianni@gmail.com>
2022-01-10 15:44:34 -0300

    :tada: first commit

---
+++ app/Http/Controllers/SeriesController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Episodio;
+use App\Http\Requests\SeriesFormRequest;
+use App\Serie;
+use App\Services\CriadorDeSerie;
+use App\Temporada;
+use Illuminate\Http\Request;
+
+class SeriesController extends Controller
+{
+    public function index(Request $request) {
+        $series = Serie::query()->orderBy('nome')->get();
+        $mensagem = $request->session()->get('mensagem');
+        $mensagem = $request->session()->remove('mensagem');
+
+        return view('series.index', compact('series', 'mensagem'));
+    }
+
+    public function create()
+    {
+        return view('series.create');
+    }
+
+    public function store(SeriesFormRequest $request, CriadorDeSerie $criadorDeSerie)
+    {
+        $serie = $criadorDeSerie->criarSerie(
+            $request->nome, 
+            $request->qtd_temporadas, 
+            $request->ep_por_temporada
+        );
+
+        $request->session()->flash('mensagem', "Série {$serie->id} e suas temporadas e episódios adicionados com sucesso {$serie->nome}");
+
+        // return redirect('/series');
+        return redirect()->route('listar_series');
+    }
+
+    public function destroy(Request $request)
+    {
+        $serie = Serie::find($request->id);
+        $nomeSerie = $serie->nome;
+        $serie->temporadas->each(function (Temporada $temporada) {
+            $temporada->episodios()->each(function (Episodio $episodio) {
+                $episodio->delete();
+            });
+            $temporada->delete();
+        });
+        $serie->delete();
+        $request->session()->flash('mensagem', "Série $nomeSerie removida com sucesso!");
+
+        return redirect()->route('listar_series');
+    }
+}
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Controle de Séries</title>
+    <!-- CSS only -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <div class="jumbotron">
+            <h1>@yield('cabecalho')</h1>
+        </div>
+
+        @yield('conteudo')
+    </div>
+</body>
+</html>
