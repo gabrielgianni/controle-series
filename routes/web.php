8f89eb2155cc5e176a91e0d5e7ffc2ed11c968e5
Gabriel <contatogabrielgianni@gmail.com>
2022-01-13 01:40:23 -0300

    :sparkles: adicionado login e registro de usuários

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,12 @@ Route::post('/series/{id}/editaNome', 'SeriesController@editaNome');
 Route::get('/series/{serieId}/temporadas', 'TemporadasController@index');
 Route::get('/temporadas/{temporada}/episodios', 'EpisodiosController@index');
 Route::post('/temporadas/{temporada}/episodios/assistir', 'EpisodiosController@assistir');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/entrar', 'EntrarController@index');
+Route::post('/entrar', 'EntrarController@entrar');
+Route::get('/registrar', 'RegistroController@create');
+Route::post('/registrar', 'RegistroController@store');
